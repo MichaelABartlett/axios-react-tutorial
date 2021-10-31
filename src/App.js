93f72fe97db4bffe5279bde8fb4c4270319eadaa
unknown <michael@machinistchoice.com>
2021-10-31 15:18:16 -0500

    saving progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let PUNK_API = 'https://api.punkapi.com/v2/beers'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      arrayOfBeer: [],
+    }
+  }
+
+
+
+  componentDidMount() {
+    //fetch(PUNK_API)
+    //.then((response) => response.json())
+    //.then((data) => this.setState({
+      //beers: data
+    //}));
+    axios.get('https://api.punkapi.com/v2/beers')
+      .then( res => {
+        const arrayOfBeer = res.data
+        this.setState({ arrayOfBeer })
+    })
+  }
+
+//componentDidUpdate(){
+//  console.log("This is from state", this.state.beer)
+//}
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <ol>{this.state.arrayOfBeer.map((beer, index) => {
+          return (
+            <li key={index} >{beer.name}</li>
+          )
+        })}</ol>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
